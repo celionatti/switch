6e434c6dd4c5f223c3b235131474acb61b3bc335
celionatti <amisuusman@gmail.com>
2026-08-14 01:19:24 +0100

    feat(kernel,skeleton): Add MySQL connection configuration block and dynamic multi-db connection booting in AppBuilder

--- skeleton/config/database.php
+++ skeleton/config/database.php
@@ -3,22 +3,48 @@
 declare(strict_types=1);
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Database Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Supported drivers: "mysql", "pgsql", "sqlite"
+    |
+    */
     'default' => env('DB_CONNECTION', 'sqlite'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    */
     'connections' => [
         'sqlite' => [
-            'driver' => 'sqlite',
+            'driver'   => 'sqlite',
             'database' => env('DB_DATABASE', __DIR__ . '/../database/database.sqlite'),
-            'prefix' => '',
+            'prefix'   => '',
         ],
+
+        'mysql' => [
+            'driver'   => 'mysql',
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => (int) env('DB_PORT', 3306),
+            'database' => env('DB_DATABASE', 'switch_db'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8mb4',
+            'prefix'   => '',
+        ],
+
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => (int) env('DB_PORT', 5432),
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => (int) env('DB_PORT', 5432),
             'database' => env('DB_DATABASE', 'switch_db'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
         ],
     ],
 ];
